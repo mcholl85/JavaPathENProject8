a9f785ce91205949d7cc1a4e9fab3a24a1fb3a5c
Matthieu Chollet <matthieuchollet@MacBook-Pro-de-Matthieu.local>
2024-05-29 18:31:36 +0200

    feat: modify getNearByAttractions to return top 5 closest attractions

---
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/dto/NearbyAttractionsDTO.java
@@ -0,0 +1,2 @@
+package com.openclassrooms.tourguide.dto;public class NearbyAttractionsDTO {
+}
--- TourGuide/src/main/java/com/openclassrooms/tourguide/service/TourGuideService.java
+++ TourGuide/src/main/java/com/openclassrooms/tourguide/service/TourGuideService.java
@@ -7,14 +7,7 @@ import com.openclassrooms.tourguide.user.UserReward;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -96,14 +89,12 @@ public class TourGuideService {
 	}
 
 	public List<Attraction> getNearByAttractions(VisitedLocation visitedLocation) {
-		List<Attraction> nearbyAttractions = new ArrayList<>();
-		for (Attraction attraction : gpsUtil.getAttractions()) {
-			if (rewardsService.isWithinAttractionProximity(attraction, visitedLocation.location)) {
-				nearbyAttractions.add(attraction);
-			}
-		}
+        List<Attraction> allAttractions = gpsUtil.getAttractions();
 
-		return nearbyAttractions;
+        return allAttractions.stream()
+                .sorted(Comparator.comparingDouble(attraction -> rewardsService.getDistance(attraction, visitedLocation.location)))
+                .limit(5)
+                .toList();
 	}
 
 	private void addShutDownHook() {
